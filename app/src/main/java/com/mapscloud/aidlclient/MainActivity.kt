16f0d4b0e0a4ef16e40c2b55307d156bb6629880
Y9000X <wangyc0129@navinfo.com>
2024-09-05 16:31:13 +0800

    新增跨进程通信接口回调形式

--- app/src/main/java/com/mapscloud/aidlclient/MainActivity.kt
+++ app/src/main/java/com/mapscloud/aidlclient/MainActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.mapscloud.aidlservice.IMyAidlInterface
+import com.mapscloud.call.CallbackInterface
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -20,13 +21,26 @@ class MainActivity : AppCompatActivity() {
 
     private var serviceConnection: ServiceConnection = object : ServiceConnection {
         override fun onServiceConnected(componentName: ComponentName, iBinder: IBinder) {
+            Log.e(TAG, "onServiceConnected: 服务连接成功")
             iMyAidlInterface = IMyAidlInterface.Stub.asInterface(iBinder)
-            Log.e(TAG, "onServiceConnected: ")
         }
 
         override fun onServiceDisconnected(componentName: ComponentName) {
-            Log.e(TAG, "onServiceDisconnected: ")
+            Log.e(TAG, "onServiceDisconnected: 服务连接失败")
         }
+    }
+
+    private fun setListener() {
+        val stub = object : CallbackInterface.Stub() {
+            override fun callback(number1: Int, number2: Int, bundle: Bundle) {
+                Log.e(TAG, "callback: number1:$number1---number2:$number2")
+                val sum = bundle.getString("result", "null")
+                Log.e(TAG, "callback: result:$sum")
+            }
+        }
+        Log.e(TAG, "setListener ---> CallbackInterface: ${stub.hashCode()}")
+        iMyAidlInterface?.setListener(1, 2, stub)
+        iMyAidlInterface?.setCallbackListener(2, 3, stub)
     }
 
     fun onBinding(view: View) {
@@ -48,23 +62,23 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("aidlclient", "我来自AIDLClient")
         startForegroundService(intent)
         bindService(intent, serviceConnection, BIND_AUTO_CREATE)
-
     }
-
 
     fun onComm(view: View) {
-        val str = iMyAidlInterface?.getStr(2)
-        Log.e(TAG, "onServiceConnected: $str")
+        val str = iMyAidlInterface?.add(2, 3)
+        Log.e(TAG, "result: $str")
+        setListener()
     }
 
-
     fun onGoing(view: View) {
-        val packageInfo = packageManager.getPackageInfo("com.mapscloud.aidlservice", 0)
+        val packageName = "com.mapscloud.aidlservice"
+        val activityService = "com.mapscloud.ui.MainActivity"
+        val packageInfo = packageManager.getPackageInfo(packageName, 0)
         if (packageInfo == null) {
             Log.e(TAG, "服务不存在")
         }
         val componentName =
-            ComponentName("com.mapscloud.aidlservice", "com.mapscloud.aidlservice.MainActivity")
+            ComponentName(packageName, activityService)
         var intent = Intent()
         intent.component = componentName
         startActivity(intent)
